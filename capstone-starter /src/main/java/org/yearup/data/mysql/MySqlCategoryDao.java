2c2c26e972120308075f96345576746931db8a77
embet-mengistu <131568421+embet-mengistu@users.noreply.github.com>
2025-06-24 13:16:57 -0700

    added the delete catgories functionality

--- capstone-starter /src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ capstone-starter /src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -117,7 +117,18 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     @Override
     public void delete(int categoryId)
     {
-        // delete category
+        String sql = "DELETE FROM categories WHERE category_id = ?";
+
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql))
+        {
+            stmt.setInt(1, categoryId);
+            stmt.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException("Error deleting category", e);
+        }
     }
 
     private Category mapRow(ResultSet row) throws SQLException
